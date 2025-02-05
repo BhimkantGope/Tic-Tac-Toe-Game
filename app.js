09ffacce380a76cde431622fede7fad1cd6b5b24
BHIMKANT GOPE <147872144+BhimkantGope@users.noreply.github.com>
2025-02-05 07:33:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,11 +68,7 @@ const checkWinner = () => {
         if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
             if(pos1Val === pos2Val && pos2Val === pos3Val){
                 showWinner(pos1Val);
-            }else{
-                msg.innerText = `Game has drow!`;
-                msgContainer.classList.remove("hide");
-                disableBoxes();
-            } 
+            }
         }
     }
 };
